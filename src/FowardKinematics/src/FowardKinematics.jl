6602303ecd2d5deb8e2207ec12118cda3194213f
Jorge Brito <jorge.brito.json@gmail.com>
2021-08-16 16:57:53 -0300

    Attribution

--- src/FowardKinematics/src/FowardKinematics.jl
+++ src/FowardKinematics/src/FowardKinematics.jl
@@ -1,3 +1,8 @@
+# This example was adapted from: Coding Challenge #64.1 "Foward Kinematics"
+# by Daniel Shiffman
+#
+# https://thecodingtrain.com/CodingChallenges/064.1-forward-kinematics.html
+
 module FowardKinematics
 
 using Lynx
@@ -9,6 +14,6 @@ Lynx.init("FowardKinematics", 800, 600)
 include("segment.jl")
 include("main.jl")
 
-run!(update, setup, await=false)
+run!(update, setup, await=true)
 
 end # FowardKinematics
